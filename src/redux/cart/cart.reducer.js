e6068e87bba1e94e2d4f034cedbeeef02b469a7f
Nitin Jha <nitin.jha6@gmail.com>
2020-04-30 11:57:06 +0530

    increase add to cart quantity

--- src/redux/cart/cart.reducer.js
+++ src/redux/cart/cart.reducer.js
@@ -1,4 +1,6 @@
 import CartActionTypes from "./cart.types";
+import "./cart.utils";
+import { addItemToCart } from "./cart.utils";
 const INITIAL_STATE = {
   hiddenValue: true,
   cartItems: [],
@@ -14,7 +16,7 @@ const cartReducer = (state = INITIAL_STATE, action) => {
     case CartActionTypes.ADD_ITEM:
       return {
         ...state,
-        cartItems: [...state.cartItems, action.payload],
+        cartItems: addItemToCart(state.cartItems, action.payload),
       };
 
     default:
