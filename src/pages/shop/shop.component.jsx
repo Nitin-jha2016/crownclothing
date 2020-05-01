c54c0139370f07e7ac329bd26054b8cbe6fc8184
Nitin Jha <nitin.jha6@gmail.com>
2020-05-01 12:06:12 +0530

    state created for categoried and collections

--- src/pages/shop/shop.component.jsx
+++ src/pages/shop/shop.component.jsx
@@ -1,20 +1,19 @@
-import React, { Component } from "react";
-import SHOP_DATA from "./shop.data";
+import React from "react";
+import { connect } from "react-redux";
 import CollectionPreview from "../../components/previewcollection/collectionpreview.component";
-class ShopPage extends Component {
-  constructor() {
-    super();
-    this.state = { collections: SHOP_DATA };
-  }
-  render() {
-    const { collections } = this.state;
-    return (
-      <div className="shop-page">
-        {collections.map(({ id, ...otherCollectionProps }) => (
-          <CollectionPreview key={id} {...otherCollectionProps} />
-        ))}
-      </div>
-    );
-  }
-}
-export default ShopPage;
+import { selectShopCollections } from "../../redux/shop/shop.selector";
+import { createStructuredSelector } from "reselect";
+const ShopPage = ({ collections }) => {
+  return (
+    <div className="shop-page">
+      {collections.map(({ id, ...otherCollectionProps }) => (
+        <CollectionPreview key={id} {...otherCollectionProps} />
+      ))}
+    </div>
+  );
+};
+const mapStateToProps = createStructuredSelector({
+  collections: selectShopCollections,
+});
+
+export default connect(mapStateToProps)(ShopPage);
