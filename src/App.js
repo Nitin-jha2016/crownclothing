3add730d53c81e4a01b5c7e0182ed4293d62fca8
Nitin Jha <nitin.jha6@gmail.com>
2020-05-19 14:04:04 +0530

    added  useraction into app.js file

--- src/App.js
+++ src/App.js
@@ -1,22 +1,27 @@
 import React, { Component } from "react";
+import { connect } from "react-redux";
 import { Route, Switch } from "react-router-dom";
 import HomePage from "./pages/homepage/homepage.component";
 import ShopPage from "./pages/shop/shop.component";
 import Header from "./components/header/header.component";
 import SignInAndSignUpPage from "./pages/sign-in-sign-up/signin-sign-up.component";
+import { setCurrentUser } from "./redux/user/user.action";
 import {
   auth,
   createUserProfileDocument,
 } from "./components/firebase/firebase-utils";
 
 import "./App.css";
+
 class App extends Component {
-  constructor() {
-    super();
-    this.state = { currentUser: null };
-  }
+  // I removed this as i am getting now as i will set currentUser state via redux function now
+  // constructor() {
+  //   super();
+  //   this.state = { currentUser: null };
+  // }
   unsubscribeFromAuth = null;
   componentDidMount() {
+    const { setCurrentUser } = this.props; // my function name
     this.unsubscribeFromAuth = auth.onAuthStateChanged(async (userAuth) => {
       console.log("User exist ?? ", userAuth);
       if (userAuth) {
@@ -26,16 +31,18 @@ class App extends Component {
           // it will take snapshot of that useref data and tell if it exist is true or false and snapshot.date() wont give u id only
           // give u data
           console.log("data is ", snapShot.id, "  ", snapShot.data());
-          this.setState({
-            currentUser: {
-              id: snapShot.id,
-              ...snapShot.data(),
-            },
-          });
+          // this.setState({
+          //   currentUser: {
+          //     id: snapShot.id,
+          //     ...snapShot.data(),
+          //   },
+          // });
+          setCurrentUser({ id: snapShot.id, ...snapShot.data() });
         });
         // console.log(this.state.currentUser);
       }
-      this.setState({ currentUser: userAuth }); //it means it is null
+      // this.setState({ currentUser: userAuth }); //it means it is null
+      setCurrentUser(userAuth);
     });
   }
   //if window close then close session as well
@@ -56,4 +63,8 @@ class App extends Component {
     );
   }
 }
-export default App;
+const mapDispatchToProps = (dispatch) => ({
+  setCurrentUser: (user) => dispatch(setCurrentUser(user)),
+});
+
+export default connect(null, mapDispatchToProps)(App);
--- src/redux/user/user.action.js
+++ src/redux/user/user.action.js
@@ -1,4 +1,4 @@
-export const SET_CURRENT_USER = (user) => ({
+export const setCurrentUser = (user) => ({
   type: "SET_CURRENT_USER",
   payload: user,
 });
