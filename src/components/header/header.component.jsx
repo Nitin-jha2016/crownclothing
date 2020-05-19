d4c093bdfe010f80b541ac4c14734be8aa502de1
Nitin Jha <nitin.jha6@gmail.com>
2020-05-19 15:27:22 +0530

    Cart Icon Added

--- src/components/header/header.component.jsx
+++ src/components/header/header.component.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import "./header.styles.scss";
 import { ReactComponent as Logo } from "../../assets/crown.svg.svg";
+import CartIcon from "../cart-icon/cart-icon.component";
 import { connect } from "react-redux"; // it is HOC which help to connect redux
 import { Link } from "react-router-dom";
 import { auth } from "../firebase/firebase-utils";
@@ -28,6 +29,7 @@ const Header = ({ currentUser }) => {
             SIGN IN
           </Link>
         )}
+        <CartIcon />
       </div>
     </div>
   );
