6ebdd3d8e53af910f5fe7e964344faa02127dafe
Nitin Jha <nitin.jha6@gmail.com>
2020-05-20 09:17:05 +0530

    added CartItems and addItem action reducer

--- src/redux/cart/cart.reducer.js
+++ src/redux/cart/cart.reducer.js
@@ -1,6 +1,7 @@
 import { CartActionTypes } from "./cart.types";
 const INITIAL_STATE = {
-  hidden: true,
+  hidden: true, // this hidden is for cart drop down
+  cartItems: [], // this is to carry items inside the cart
 };
 
 export const cartReducer = (state = INITIAL_STATE, action) => {
@@ -9,6 +10,12 @@ export const cartReducer = (state = INITIAL_STATE, action) => {
       return {
         ...state,
         hidden: !state.hidden,
+      };
+
+    case CartActionTypes.ADD_ITEM:
+      return {
+        ...state,
+        cartItems: [...state.cartItems, action.payload], // so existing cart item and adding new payload or item
       };
 
     default:
