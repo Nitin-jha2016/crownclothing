32bd780278289b737c71141e91f4b9de4af5adce
Nitin Jha <nitin.jha6@gmail.com>
2020-05-21 12:00:32 +0530

    converted collections array to object

--- src/redux/shop/shop.selector.js
+++ src/redux/shop/shop.selector.js
@@ -1,12 +1,13 @@
 import { createSelector } from "reselect";
+// not reuquie below as now we convered shop data to object from array
 
-const COLLECTION_ID_MAP = {
-  hats: 1,
-  sneakers: 2,
-  jackets: 3,
-  womens: 4,
-  mens: 5,
-};
+// const COLLECTION_ID_MAP = {
+//   hats: 1,
+//   sneakers: 2,
+//   jackets: 3,
+//   womens: 4,
+//   mens: 5,
+// };
 
 const selectStop = (state) => state.shop;
 
@@ -16,8 +17,11 @@ export const selectCollections = createSelector(
 );
 
 export const selectCollection = (collectionUrlParam) =>
-  createSelector([selectCollections], (collections) =>
-    collections.find(
-      (collection) => collection.id === COLLECTION_ID_MAP[collectionUrlParam]
-    )
+  createSelector(
+    [selectCollections],
+    (collections) =>
+      // collections.find(
+      //   (collection) => collection.id === COLLECTION_ID_MAP[collectionUrlParam]
+      // )
+      collections[collectionUrlParam]
   );
